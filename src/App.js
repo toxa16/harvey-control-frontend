880586667ad1877b50c0f4599c6a84b1ab355cf6
toxa16 <toxa16@gmail.com>
2019-10-22 19:11:40 +0300

    init subapp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import './App.css';
+import MainScreen from './subapp/components/MainScreen';
 
 function App() {
   return (
     <div className="App">
       App
+      <MainScreen />
     </div>
   );
 }
