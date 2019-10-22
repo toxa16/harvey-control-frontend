bb20ed5ae30d5052f4771433dc667c1ee990b8e4
toxa16 <toxa16@gmail.com>
2019-10-22 19:25:37 +0300

    displaying screens due to connection status

--- src/subapp/components/MainScreen.js
+++ src/subapp/components/MainScreen.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 import DisconnectedScreen from './DisconnectedScreen';
+import ConnectionStatus from '../redux-state/connection-status.enum';
+import ConnectingScreen from './ConnectingScreen';
+import ConnectedScreen from './ConnectedScreen';
+import DisconnectingScreen from './DisconnectingScreen';
+import ErrorScreen from './ErrorScreen';
 
 export default function MainScreen({ status }) {
   function renderByStatus() {
     switch (status) {
-      default: return <DisconnectedScreen />
+      case ConnectionStatus.CONNECTING: {
+        return <ConnectingScreen />;
+      }
+      case ConnectionStatus.CONNECTED: {
+        return <ConnectedScreen />;
+      }
+      case ConnectionStatus.DISCONNECTING: {
+        return <DisconnectingScreen />;
+      }
+      case ConnectionStatus.ERROR: {
+        return <ErrorScreen />
+      }
+      default: return <DisconnectedScreen />;
     }
   }
 
